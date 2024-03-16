f9e7ce2637a25a836d2dc06b20a2e1c6fe76c231
Sharon Shamay <84649681+sharonLmao@users.noreply.github.com>
2024-03-16 18:22:19 +0200

    Added AI Thinker ESP32-CAM Support + vscode settings + devcontainer + CMakeLists + Windows idf.py Launcher
    Added AI Thinker ESP32-CAM Support + vscode settings + devcontainer + CMakeLists + Windows idf.py Launcher
    To use the launcher just run `esp` from the terminal
    Notice the project name (parent folder name) was engine-rtc and the CMakeLists.txt contain this name for some reason
    Enjoy!

--- examples/esp32/README.md
+++ examples/esp32/README.md
@@ -5,6 +5,7 @@ Stream JPEG over WebRTC datachannel with ESP32.
 
 | Hardware ||
 |---|---|
+| [ESP32-CAM]
 | [ESP32-EYE](https://github.com/espressif/esp-who/blob/master/docs/en/get-started/ESP-EYE_Getting_Started_Guide.md) |<img src="https://www.espressif.com/sites/default/files/esp-eye-2-190116.png" width="64">|
 | [ESP32 M5Camera](https://github.com/m5stack/M5Stack-Camera) |<img src="https://static-cdn.m5stack.com/resource/docs/products/unit/m5camera/m5camera_01.webp" width="64">|
 
--- examples/esp32/main/camera.c
+++ examples/esp32/main/camera.c
@@ -51,6 +51,23 @@ static const char *TAG = "Camera";
 #define CAM_PIN_VSYNC 25
 #define CAM_PIN_HREF 26
 #define CAM_PIN_PCLK 21
+#elif defined(CONFIG_ESP32_CAM)
+#define CAM_PIN_PWDN -1
+#define CAM_PIN_RESET -1
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
 #else // ESP32-EYE
 #define CAM_PIN_PWDN -1
 #define CAM_PIN_RESET -1
